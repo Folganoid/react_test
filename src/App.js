7a90f6d7b30e19f918a6f444083ee84d2eff56fc
Andrii Holubkov <folganoid@yahoo.com>
2023-02-24 11:49:44 +0200

    feat: cart

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      orders: [],
       items: [
         {
           id: 1,
@@ -52,16 +53,32 @@ class App extends React.Component {
         }
       ]
     }
+    this.addToOrder = this.addToOrder.bind(this);
+    this.deleteOrder = this.deleteOrder.bind(this);
   }
   render() {
     return (
       <div className="wrapper">
-        <Header />
-        <Items items={this.state.items}/>
+        <Header onDelete={this.deleteOrder} orders={this.state.orders}/>
+        <Items items={this.state.items} onAdd={this.addToOrder}/>
         <Footer />
       </div>
     )
   };
+
+  deleteOrder(id) {
+    this.setState({orders: this.state.orders.filter(el => el.id !== id)});
+  }
+
+  addToOrder(item) {
+    let isInArr = false;
+    this.state.orders.forEach(el => {
+      if(el.id === item.id) {
+        isInArr = true;
+      }
+    })
+    if (!isInArr) this.setState({orders: [...this.state.orders, item]});
+  }
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,49 @@
 import React, { useState } from 'react';
 import { FaShoppingCart } from 'react-icons/fa';
+import Order from './Order';
 
-export default function Header() {
+const showOrders = (props) => {
+  let summa = props.orders.reduce((a, b) => a + Number.parseFloat(b.price), 0);
+  return (
+    <div>
+      {props.orders.map(el => (
+        <Order key={el.id} item={el} onDelete={props.onDelete}/>
+      ))}
+      <p className='summa'>Total: {summa.toFixed(2)}$</p>
+    </div>
+  )
+}
+
+const showNothing = () => {
+  return (<div className='empty'>
+    <h2>Empty cart</h2>
+  </div>)
+}
+
+export default function Header(props) {
 
   let [cartOpen, setCartOpen] = useState(false);
 
   return (
     <header>
-        <div>
-            <span className='Logo'>House staff</span>
-            <ul className='nav'>
-                <li>About us</li>
-                <li>Contacts</li>
-                <li>Cabinet</li>
+      <div>
+        <span className='Logo'>House staff</span>
+        <ul className='nav'>
+          <li>About us</li>
+          <li>Contacts</li>
+          <li>Cabinet</li>
 
-            </ul>
-            <FaShoppingCart onClick = {() => setCartOpen(cartOpen = !cartOpen)} className={`shop-cart-button ${cartOpen && 'active'}`}/>
-            {cartOpen && (
-                <div className='shop-cart'>
-                    
-                </div>
-            )}
-        </div>    
-        <div className='presentation'></div>
+        </ul>
+        <FaShoppingCart onClick={() => setCartOpen(cartOpen = !cartOpen)} className={`shop-cart-button ${cartOpen && 'active'}`} />
+        {cartOpen && (
+          <div className='shop-cart'>
+            {props.orders.length > 0 ?
+              showOrders(props) : showNothing()
+            }
+          </div>
+        )}
+      </div>
+      <div className='presentation'></div>
     </header>
   )
 }
