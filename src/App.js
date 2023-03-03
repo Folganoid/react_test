6d05441a238fdc2046ff946c9069c8da228c40ac
Andrii Holubkov <folganoid@yahoo.com>
2023-03-03 18:02:01 +0200

    counts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { useState } from "react";
+
 function App() {
+  const [likes, setLikes] = useState(0);
+  const [val, setVal] = useState("ljlkjdflkslk");
+
+  function inc() {
+    setLikes(likes + 1);
+  }
+
+  function dec() {
+    setLikes(likes - 1);
+  }
+
   return (
     <div className="App">
-      TEST
+      <h1>{likes}</h1>
+      <h1>{val}</h1>
+      <input type="text" value={val} onChange={event => setVal(event.target.value)}/>
+      <button onClick={inc}>+</button>
+      <button onClick={dec}>-</button>
     </div>
   );
 }
