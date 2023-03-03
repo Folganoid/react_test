ee7b118a2a91e14fe0704f70d94062674233e1d3
Andrii Holubkov <folganoid@yahoo.com>
2023-03-03 17:28:45 +0200

    init: reset

--- src/hooks/useActions.ts
+++
@@ -1,8 +0,0 @@
-import { useDispatch } from "react-redux"
-import { bindActionCreators } from "redux";
-import ActionCreator from '../store/action-creators/';
-
-export const UseActions = () => {
-    const dispatch = useDispatch();
-    return bindActionCreators(ActionCreator, dispatch);
-}
