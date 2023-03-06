ef2a5fa4ff0b0b7088d3d9f455bcfcfd18989cab
Andrii Holubkov <folganoid@yahoo.com>
2023-03-06 12:17:58 +0200

    loader

--- src/App.js
+++ src/App.js
@@ -1,36 +1,38 @@
-import { useMemo, useState } from "react";
+import { useEffect, useState } from "react";
+import PostService from "./API/PostService";
 import PostFilter from "./components/PostFilter";
 import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
+import Loader from "./components/UI/loader/Loader";
 import MyModal from "./components/UI/MyModal/MyModal";
+import { usePosts } from "./hooks/usePosts";
 import './styles/App.css';
 
 function App() {
 
-  const [posts, setPosts] = useState([
-    {id: 1, title: 'JS', body: 'jjjj'},
-    {id: 2, title: 'TS', body: 'aaa'},
-    {id: 3, title: 'Go', body: 'gogogo'}
-  ])
+  const [posts, setPosts] = useState([]);
 
   const [filter, setFilter] = useState({sort: '', query: ''});
   const [modal, setModal] = useState(false);
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+  const [isPostsLoading, setIsPostsLoading] = useState(false);
 
-  const sortedPosts = useMemo(() => {
-    if (filter.sort) {
-      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
-    }
-    return posts;
-  }, [filter.sort, posts]);
+  useEffect(() => {
+    fetchPosts();
+  }, [])
 
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter(post => post.title.toLocaleLowerCase().includes(filter.query))
-  }, [filter.query, sortedPosts]);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
     setModal(false);
+  }
+
+  async function fetchPosts() {
+    setIsPostsLoading(true);
+    const posts = await PostService.getAll();
+    setPosts(posts);
+    setIsPostsLoading(false);
   }
 
   const removePost = (post) => {
@@ -39,6 +41,7 @@ function App() {
 
   return (
     <div className="App">
+      <button onClick={fetchPosts}>GET POSTS</button>
       <MyButton style={{alignSelf: 'start'}} onClick={() => setModal(true)}>
         Create post
       </MyButton>
@@ -47,7 +50,11 @@ function App() {
       </MyModal>
       <hr style={{margin: "15px 0"}}/>
       <PostFilter filter={filter} setFilter={setFilter}/>
-      <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Post List #1"/> 
+      {isPostsLoading 
+        ? <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}><Loader/></div>
+        : <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Post List #1"/> 
+      }
+      
     </div>
   );
 }
